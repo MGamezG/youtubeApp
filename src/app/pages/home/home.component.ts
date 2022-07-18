1b78375bbc10f888d0722411ded639e4fd9747fa
MGamezG <l171130314@gamadero.tecnm.mx>
2022-07-18 18:41:06 -0500

    cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,22 @@
 import { YoutbeResponse } from './../../models/youtube.models';
 import { YoutubeService } from './../../services/youtube.service';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  videos:any[]=[]
 
   constructor( private youtubeServices:YoutubeService) { }
 
   ngOnInit(){
    this.youtubeServices.getVideos().subscribe(
-    (response:YoutbeResponse) =>{
+    (response:any) =>{
     console.log(response);
+    this.videos.push(...response);
+    console.warn(this.videos);
    } );
 
   }
